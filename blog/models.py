a8026eea7d00959959a41724572ce30e6864840e
Burnnet <burnnet00@gmail.com>
2023-04-02 17:12:01 +0300

    Started blog

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Post(models.Model):
+    '''Data post'''
+    title = models.CharField('Заголовок записи', max_length=100)
+    description = models.TextField('Текст записи')
+    autor = models.CharField('Имя автора', max_length=100)
+    date = models.DateField('Дата публикации')
+    image = models.ImageField('Изображения', upload_to='image/%Y')
+
+    def __str__(self):
+        return f'{self.title}, {self.autor}'
+
+    class Meta:
+        verbose_name = 'Запись'
