7e5118a5337b5c378d6544cc38a94a7bc628ec78
Burnnet <Burnnet00@mail.ru>
2023-05-20 18:52:16 +0300

    add form in dbase

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.PostView.as_view()),
-               path('<int:pk>/', views.PostDetail.as_view())]
+urlpatterns = [
+    path('', views.PostView.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('review/<int:pk>/', views.AddComment.as_view(), name='add_comments'),
+
+    ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from .models import Post
+from .form import CommentsForm
 
 class PostView(View):
     '''Vievs'''
@@ -9,6 +10,17 @@ class PostView(View):
         return render(reguest, 'blog/blog.html', {'post_list': posts})
 
 class PostDetail(View):
+    '''Viev'''
     def get (self, reguest, pk):
         post = Post.objects.get(id=pk)
         return render(reguest, 'blog/blog_detail.html', {'post': post})
+
+class AddComment(View):
+    '''add comments'''
+    def post(self, request, pk):
+        form = CommentsForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.post_id = pk
+            form.save()
+        return redirect(f'/{pk}')
