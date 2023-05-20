dc36a3ef15cd72933633c96282927be884ec0358
Burnnet <Burnnet00@mail.ru>
2023-05-20 16:51:25 +0300

    add comments

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('', views.PostView.as_view()),
-               path('<int:pk>/', views.PostDetail.as_view )]
+               path('<int:pk>/', views.PostDetail.as_view())]
